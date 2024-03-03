7628b6e3f407070ec34f7a001b965d3b0449d0f3
hapa-magic <157315515+hapaMagic@users.noreply.github.com>
2024-03-02 19:31:29 -0800

    Update Enemy.cs

--- Proj1/Assets/Scripts/Enemy.cs
+++ Proj1/Assets/Scripts/Enemy.cs
@@ -9,10 +9,10 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float _jumpTime = 3f;
     [SerializeField] private float _initVelocity = 1.5f;
     private Transform _enemySprite;
-    private GameObject _enemyShadow;
+    private Transform _enemyShadow;
     private Animator _anim;
     private BoxCollider2D _collider;
-    private Player _player;
+    private Transform _player;
     private AudioSource _audioSource;
     private bool _canDamage;
     private bool _alwaysOn;
@@ -21,16 +21,16 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _anim = GetComponent<Animator>();
+        //_anim = GetComponent<Animator>();
         _collider = GetComponent<BoxCollider2D>();
-        _player = GameObject.Find("Player").GetComponent<Player>();
+        _player = GameObject.Find("Player").transform;
         Debug.Log(_player);
-        _audioSource = GetComponent<AudioSource>();
+        //_audioSource = GetComponent<AudioSource>();
         _enemySprite = this.gameObject.transform.GetChild(0);
+        _enemyShadow = this.gameObject.transform.GetChild(1);
         _canDamage = true;
         _alwaysOn = true;
         StartCoroutine(enemyMovement());
-
     }
 
     // Update is called once per frame
@@ -45,21 +45,22 @@ public class Enemy : MonoBehaviour
     // Post: enemy moves towards player
     IEnumerator enemyMovement() {
         while (_alwaysOn) {
-        Vector3 whereToMove = _player.transform.position - transform.position;
-        float savedTime = Time.time;
-        while (Time.time - savedTime >= _jumpTime) {
-            _canDamage = false;
-            transform.Translate(whereToMove * _speed * Time.deltaTime);
-            _enemySprite.transform.Translate(Vector3.up * Time.deltaTime * _enemyJumpSpeed);
-        }
-        new WaitForSeconds(1f);
-        savedTime = Time.time;
-        while (_enemySprite.transform.position.y > _enemyShadow.transform.position.y) {
-            float accel = Mathf.Pow(_initVelocity, Time.time - savedTime);
-            _enemySprite.transform.Translate(Vector3.down * accel * Time.deltaTime);
-        }
-        _canDamage = true;
-        new WaitForSeconds(2f);
+            Vector3 whereToMove = _player.position - transform.position;
+            Debug.Log(whereToMove);
+            float savedTime = Time.time;
+            while (Time.time - savedTime >= _jumpTime) {
+                _canDamage = false;
+                transform.Translate(whereToMove * _speed * Time.deltaTime);
+                _enemySprite.transform.Translate(Vector3.up * Time.deltaTime * _enemyJumpSpeed);
+            }
+            new WaitForSeconds(1f);
+            savedTime = Time.time;
+            while (_enemySprite.position.y > _enemyShadow.position.y) {
+                float accel = Mathf.Pow(_initVelocity, Time.time - savedTime);
+                _enemySprite.transform.Translate(Vector3.down * accel * Time.deltaTime);
+            }
+            _canDamage = true;
+            new WaitForSeconds(2f);
         }
         yield return new WaitForSeconds(2f);
 
