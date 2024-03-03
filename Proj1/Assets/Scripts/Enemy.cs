8ee3ad73021244d780625a90b9c037efbb7e832f
hapa-magic <157315515+hapaMagic@users.noreply.github.com>
2024-03-02 21:13:30 -0800

    EnemyMovement is finally working!!!!!

--- Proj1/Assets/Scripts/Enemy.cs
+++ Proj1/Assets/Scripts/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float _speed = 2.0f;
+    [SerializeField] private float _speed;
     [SerializeField] private float _enemyJumpSpeed = 1.0f;
     [SerializeField] private float _jumpTime = 3f;
     [SerializeField] private float _initVelocity = 1.5f;
@@ -12,6 +12,7 @@ public class Enemy : MonoBehaviour
     private Transform _enemyShadow;
     private Animator _anim;
     private BoxCollider2D _collider;
+    private GameObject _playerGO;
     private Transform _player;
     private AudioSource _audioSource;
     private bool _canDamage;
@@ -22,12 +23,12 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         //_anim = GetComponent<Animator>();
-        _collider = GetComponent<BoxCollider2D>();
-        _player = GameObject.Find("Player").transform;
-        Debug.Log(_player);
+        //_collider = GetComponent<BoxCollider2D>();
+        _playerGO = GameObject.Find("Player");
+        _player = _playerGO.transform;
         //_audioSource = GetComponent<AudioSource>();
-        _enemySprite = this.gameObject.transform.GetChild(0);
-        _enemyShadow = this.gameObject.transform.GetChild(1);
+        _enemySprite = transform.GetChild(0);
+        _enemyShadow = transform.GetChild(1);
         _canDamage = true;
         _alwaysOn = true;
         StartCoroutine(enemyMovement());
@@ -48,21 +49,22 @@ public class Enemy : MonoBehaviour
             Vector3 whereToMove = _player.position - transform.position;
             Debug.Log(whereToMove);
             float savedTime = Time.time;
-            while (Time.time - savedTime >= _jumpTime) {
+            while (Time.time - savedTime <= _jumpTime) {
                 _canDamage = false;
-                transform.Translate(whereToMove * _speed * Time.deltaTime);
+                transform.Translate(whereToMove.normalized * _speed * Time.deltaTime);
                 _enemySprite.transform.Translate(Vector3.up * Time.deltaTime * _enemyJumpSpeed);
+                yield return new WaitForSeconds(0.001f);
             }
-            new WaitForSeconds(1f);
+            yield return new WaitForSeconds(1f);
             savedTime = Time.time;
             while (_enemySprite.position.y > _enemyShadow.position.y) {
                 float accel = Mathf.Pow(_initVelocity, Time.time - savedTime);
                 _enemySprite.transform.Translate(Vector3.down * accel * Time.deltaTime);
             }
             _canDamage = true;
-            new WaitForSeconds(2f);
+            yield return new WaitForSeconds(2f);
         }
-        yield return new WaitForSeconds(2f);
-
     }
+
+
 }
